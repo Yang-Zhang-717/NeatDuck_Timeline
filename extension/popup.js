8d5483df52bed5d895eab0e2f692262c47c2da36
Yang-Zhang-717 <u244338@bcm.edu>
2026-06-17 02:30:28 -0500

    Clear repository

--- extension/popup.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("open-standalone").addEventListener("click",()=>chrome.tabs.create({url:chrome.runtime.getURL("standalone.html?src=popup")}));
-document.getElementById("open-leekduck").addEventListener("click",()=>chrome.tabs.create({url:"https://leekduck.com/events/"}));
